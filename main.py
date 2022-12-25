220a1a7dc663fdf24bee2d1d995bdfb8a56ed8df
waruingugi <waruingugientp@gmail.com>
2022-12-26 02:16:42 +0300

    Class dependecies

--- main.py
+++ main.py
@@ -1,25 +1,52 @@
-from datetime import datetime
-
-
-from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
-
-
-fake_db = {}
-
-
-class Item(BaseModel):
-    title: str
-    timestap: datetime
-    description: str | None = None
-
+# Dependancies
+from fastapi import Depends, FastAPI
 
 app = FastAPI()
 
 
-@app.put("/items/{id}")
-def update_item(id: str, item: Item):
-    json_compatible_item_data = jsonable_encoder(item)
-    fake_db[id] = json_compatible_item_data
+# async def common_parameters(
+#     q: str | None = None,
+#     skip: int = 0,
+#     limit: int = 100
+# ):
+#     return {"q": q, "skip": skip, "limit": limit}
 
+
+# @app.get("/items/")
+# async def read_items(commons: dict = Depends(common_parameters)):
+#     return commons
+
+
+# @app.get("/users/")
+# async def read_users(commons: dict = Depends(common_parameters)):
+#     return commons
+
+
+# Classes as Dependencies
+fake_items_db = [
+    {"item_name": "Foo"}, {"item_name": "Bar"},
+    {"item_name": "Baz"}
+]
+
+
+class CommonQueryParams:
+    def __init__(
+        self, q: str | None = None, skip: int = 0, limit: int = 100
+    ):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
+
+    
+@app.get("/items/")
+async def read_items(commons = Depends(CommonQueryParams)):
+    response = {}
+    if commons.q:
+        response.update(
+            {"q": commons.q}
+        )
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+
+    response.update({"items": items})
+
+    return response
