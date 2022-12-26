1f7a6b3900cab3b168ec67a0031730c75eba8eac
waruingugi <waruingugientp@gmail.com>
2022-12-26 22:31:17 +0300

    CORS

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-# Middleware
-import time
-from fastapi import FastAPI, Request
+# CORS
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    start_time = time.time()
-    response = await call_next(request)
+origins = [
+     "https://www.example.com",
+]
 
-    process_time = time.time() - start_time
-    response.headers["X-Process-Time"] = str(process_time)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
-    return response
-
-
-@app.get("/items/")
-async def get_items():
-    return {
-        "A": 1, "B": 2
-    }
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
